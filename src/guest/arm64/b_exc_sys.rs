20694d3bdacdc319d23898c8ec4a2ea777066a3a
KireinaHoro <i@jsteward.moe>
2020-05-12 22:59:25 +0800

    implement comp_b_imm

--- src/guest/arm64/b_exc_sys.rs
+++ src/guest/arm64/b_exc_sys.rs
@@ -65,4 +65,35 @@ pub fn disas_uncond_b_imm<R: HostStorage>(
     Err(DisasException::Branch(Some(addr), None))
 }
 
-disas_stub![comp_b_imm, test_b_imm, system, exc, uncond_b_reg];
+pub fn disas_comp_b_imm<R: HostStorage>(
+    ctx: &mut Arm64GuestContext<R>,
+    insn: InsnType,
+) -> Result<(), DisasException> {
+    let sf = extract(insn, 31, 1) == 1;
+    let op = extract(insn, 24, 1) == 1;
+    let rt = extract(insn, 0, 5);
+    let addr = (ctx.curr_pc() as i64 + sextract(insn as i64, 5, 19) * 4) as usize;
+
+    let next_pc = ctx.alloc_u64(ctx.next_pc() as u64);
+    let addr_val = ctx.alloc_u64(addr as u64);
+
+    let cmp = read_cpu_reg(ctx, rt as usize, sf);
+    let label_match = ctx.alloc_label();
+    let zero = ctx.alloc_u64(0);
+
+    Op::push_brc(
+        ctx,
+        &label_match,
+        &cmp,
+        &zero,
+        if op { CondOp::NE } else { CondOp::EQ },
+    );
+    do_end_tb_to_addr(ctx, &next_pc, true); // branch not taken
+
+    Op::push_setlbl(ctx, &label_match);
+    do_end_tb_to_addr(ctx, &addr_val, false); // branch taken
+
+    Err(DisasException::Branch(Some(addr), Some(ctx.next_pc())))
+}
+
+disas_stub![test_b_imm, system, exc, uncond_b_reg];
