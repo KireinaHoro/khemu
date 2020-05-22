3916fde4ca7bd7e7f1ed3c7cb97618a5718c5c16
KireinaHoro <i@jsteward.moe>
2020-05-22 19:13:45 +0800

    implement uncond_b_reg

--- src/guest/arm64/b_exc_sys.rs
+++ src/guest/arm64/b_exc_sys.rs
@@ -96,4 +96,56 @@ pub fn disas_comp_b_imm<R: HostStorage>(
     Err(DisasException::Branch(Some(addr), Some(ctx.next_pc())))
 }
 
-disas_stub![test_b_imm, system, exc, uncond_b_reg];
+pub fn disas_uncond_b_reg<R: HostStorage>(
+    ctx: &mut Arm64GuestContext<R>,
+    insn: InsnType,
+) -> Result<(), DisasException> {
+    let opc = extract(insn, 21, 4);
+    let op2 = extract(insn, 16, 5);
+    let op3 = extract(insn, 10, 6);
+    let rn = extract(insn, 5, 5) as usize;
+    let op4 = extract(insn, 0, 5);
+
+    let dst;
+
+    if op2 != 0x1f {
+        return unallocated(ctx, insn);
+    }
+
+    match opc {
+        0 | 1 | 2 => {
+            // br, blr, ret
+            match op3 {
+                0 => {
+                    // br, blr, ret
+                    if op4 != 0 {
+                        return unallocated(ctx, insn);
+                    }
+                    dst = ctx.reg(rn);
+                }
+                _ => return unallocated(ctx, insn), // pauth unimplemented
+            }
+            if opc == 1 {
+                // blr: load return address
+                let ret_addr = ctx.alloc_u64(ctx.next_pc() as u64);
+                let x30 = ctx.reg(30);
+                Op::push_mov(ctx, &x30, &ret_addr);
+            }
+            do_end_tb_to_addr(ctx, &dst, false);
+        }
+        8 | 9 => return unallocated(ctx, insn), // pauth unimplemented
+        4 => return unallocated(ctx, insn),     // eret impossible in EL0
+        5 => {
+            // drps
+            if op3 != 0 || op4 != 0 || rn != 0x1f {
+                return unallocated(ctx, insn);
+            }
+            unreachable!("DRPS not supported");
+        }
+        _ => return unallocated(ctx, insn),
+    }
+
+    Ok(())
+}
+
+disas_stub![test_b_imm, system, exc];
