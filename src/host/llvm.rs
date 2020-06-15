c1b7cbbd4636ba0ddac2993f75923ec7df76d2ff
KireinaHoro <i@jsteward.moe>
2020-06-15 18:22:14 +0800

    implement extrs

--- src/host/llvm.rs
+++ src/host/llvm.rs
@@ -117,7 +117,30 @@ impl CodeGen<LLVMHostStorage<'static>> for LLVMHostContext<'static> {
             _ => panic!("not implemented"),
         };
         store_result!(self, rd, result);
+    }
 
+    fn gen_extrs(&mut self, rd: Reg, rs: Reg, ofs: Reg, len: Reg) {
+        let i64_type = self.context.i64_type();
+        let rs = match *rs.storage.borrow() {
+            LLVMHostStorage::Empty => panic!("rs == Empty"),
+            LLVMHostStorage::Global(v) => self
+                .builder
+                .build_load(v.as_pointer_value(), "")
+                .into_int_value(),
+            LLVMHostStorage::IntV(v) => v,
+            _ => unimplemented!(),
+        };
+
+        let ofs = ofs.storage.borrow().try_as_u64().unwrap();
+        let len = len.storage.borrow().try_as_u64().unwrap();
+        let left_shift = i64_type.const_int(64 - len - ofs, false);
+        let right_shift = i64_type.const_int(64 - len, false);
+
+        let chop_high = self.builder.build_left_shift(rs, left_shift, "");
+        let result = self
+            .builder
+            .build_right_shift(chop_high, right_shift, true, "");
+        store_result!(self, rd, result);
     }
 }
 
